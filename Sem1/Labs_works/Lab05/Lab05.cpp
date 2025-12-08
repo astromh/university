818a9caa1c51372520695c81e25ca565e78546fa
AstroMh <mohammad_hemmat84@yahoo.com>
2025-12-09 00:25:59 +0300

    Finished all the three tasks

--- Sem1/Labs_works/Lab05/Lab05.cpp
+++ Sem1/Labs_works/Lab05/Lab05.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 int ReadPositiveInt(const string& prompt, int MaxValue);
+void TransformDifferences(int** A, int N, int M);
 int ReadIntElement(const string& prompt);
+long long NewSum(int** A, int N, int M);
 int OriginalSum(int** A, int N, int M);
 
 int main()
@@ -35,10 +37,7 @@ int main()
         }
     }
 
-    int Sum = OriginalSum(Arr, N, M);
-    cout << "\nThe sum of the original matrix is: " << Sum << "\n";
-
-    cout << "\nMatrix:\n";
+    cout << "\nOriginal Matrix:\n";
     for (int i=0; i<N; ++i){
         for (int j=0; j<M; ++j){
             cout << Arr[i][j] << "\t";
@@ -46,12 +45,52 @@ int main()
         cout << "\n";
     }
 
+    int Sum = OriginalSum(Arr, N, M);
+    cout << "\nThe sum of the original matrix is: " << Sum << "\n";
+
+    TransformDifferences(Arr, N, M);
+    cout << "\nTransformed Matrix:\n";
+    for (int i=0; i<N; ++i){
+        for (int j=0; j<M; ++j){
+            cout << Arr[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+
+    long long TransformedSum = NewSum(Arr, N, M);
+    cout << "\nThe sum of the new translformed matrix is: " << TransformedSum << "\n";
+
     for (int i=0; i<N; ++i){
         delete[] Arr[i];
     }
     delete[] Arr;
 
     return 0;
+}
+
+void TransformDifferences(int** A, int N, int M){
+    int total = N*M;
+
+    for (int k=0; k<total-1; ++k){
+        int i = k/M; //Row of element k
+        int j = k%M; //Column of element k
+        int kp = k+1; //Index of the next element
+        int ni = kp/M; //Row of the next element
+        int nj = kp%M; //Column of the next element
+
+        A[i][j] = A[i][j] - A[ni][nj];
+
+    }
+}
+
+long long NewSum(int** A, int N, int M){
+    long long new_sum = 0;
+    for (int i=0; i<N; ++i){
+        for (int j=0; j<M; ++j){
+            new_sum += A[i][j];
+        }
+    }
+    return new_sum;
 }
 
 int OriginalSum(int** A, int N, int M){
